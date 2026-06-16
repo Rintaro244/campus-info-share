3d6753483b4145f6cb233043e665a57da893b800
SneakyGumiz <al24052@shibaura-it.ac.jp>
2026-06-16 13:59:31 +0900

    仮

---
+++ lib/past_exam.dart
@@ -0,0 +1,16 @@
+class PastExam {
+  String pastexamId; //PastExamID
+  String title;
+  int year;
+  String subjectName;
+  String professorName;
+
+  PastExam({
+    required this.pastexamId,
+    required this.title,
+    required this.year,
+    required this.subjectName,
+    required this.professorName,
+  });
+
+}
