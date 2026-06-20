3d33cefe4f66269f0f177482623e7c3c3c0c1ade
Rintaro Suzuki <al24001@shibaura-it.ac.jp>
2026-06-21 01:54:46 +0900

    flutterfire configure によるFirebase接続設定を追加
    ・main.dart に Firebase 初期化を追加
    
    ・android等の自動生成ファイル、firebase.json を含む
    
    ※生成ファイルの共有可否は後で班と相談

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'features/spot/spot_search_screen.dart';
 
 // firebase_options.dart は .gitignore 対象のため、
@@ -7,7 +8,9 @@ import 'features/spot/spot_search_screen.dart';
 // DefaultFirebaseOptions.currentPlatform を options に渡すこと。
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
