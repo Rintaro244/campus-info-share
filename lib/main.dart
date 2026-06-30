57712f6bbe5909f25827ae712dcdcb04be658b62
SneakyGumiz <al24052@shibaura-it.ac.jp>
2026-06-30 16:11:19 +0900

    main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-// 💡 同じフォルダにある過去問一覧画面をインポート
-import 'past_exam_screen.dart'; 
+import 'mainscreen.dart';
 
-void main() async {
-  // FlutterのシステムとFirebaseを安全に繋ぐための呪文
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  
-  /*
-  try {
-    //すでにプロジェクト側でFirebaseの設定ファイル（firebase_options.dart）などが
-    // 導入されている場合は、これでFirebaseが初期化
-    await Firebase.initializeApp();
-  } catch (e) {
-    // もしFirebaseの初期化設定（flutterfire configureなど）がまだ終わっていなくても、
-    print("Firebase初期化未完了(テスト実行を継続します): $e");
-  }
-  */
 
   runApp(
     const ProviderScope(
@@ -27,20 +13,18 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: '学内情報共有アプリ テスト',
-      debugShowCheckedModeBanner: false, // 画面右上の「DEBUG」リボンを非表示にする
+      title: '学内情報共有アプリ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        useMaterial3: true, // モダンなデザインを適用
       ),
-      // 💡 アプリを起動した瞬間に、いきなり「過去問一覧画面」が開くように設定！
-      home: const PastExamListScreen(),
+      home: const MainScreen(), //first show
     );
   }
 }
